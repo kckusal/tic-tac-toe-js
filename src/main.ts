4dfdee35b134528b4eccbf68fb5fdd78579604d0
Kusal K C <kusal.kc@student.torrens.edu.au>
2024-04-13 21:30:07 +1000

    feat: implement simple Calculator app

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import { Calculator } from "./app/Calculator";
+
+function runApp() {
+	const calculator = new Calculator();
+
+	console.log("Add: 1 + 2 = ", calculator.add(1, 2));
+
+	console.log("Subtract: 1 - 2 = ", calculator.subtract(1, 2));
+
+	console.log("Multiply: 1 * 2 =", calculator.multiply(1, 2));
+
+	console.log("Divide: 1 / 2 =", calculator.divide(1, 2));
+}
+
+runApp();
